4bbd758c529c939cd460ec7fb2faf3eed5d22310
Joel German <joelgerman@192.168.1.3>
2025-01-23 18:46:09 -0400

    open, close modal

--- src/reducers/budget-reducers.tsx
+++ src/reducers/budget-reducers.tsx
@@ -1,15 +1,18 @@
 
 
 export type BudgetActions = 
-    {type: 'add-budget', payload: {budget:number}} 
+    {type: 'add-budget', payload: {budget:number}} |
+    {type: 'show-modal'} |
+    {type: 'close-modal'}
 
 export type BudgetState = {
     budget: number
-}    
+   modal: boolean 
+}
 
 export const initialBudgetState: BudgetState = {
     budget: 0,
-
+    modal: false
 }
 
 export const budgetReducer = (
@@ -21,6 +24,16 @@ export const budgetReducer = (
             return {
                 ...state,
                 budget: action.payload.budget
+            }
+        case 'show-modal':
+            return {
+                ...state,
+                modal: true
+            }
+        case 'close-modal':
+            return {
+                ...state,
+                modal: false
             }
         default:
             return state;
